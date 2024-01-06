b74dd13fa83a51e3aad4b06f500375bd77ffe189
Tarasukk <relaxsonik@gmail.com>
2024-01-06 11:35:03 +0200

    Added search ability and a little design

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,36 @@
 class MainController < ApplicationController
+  before_action :load_data, only: [:search]
+  helper :all
+
   def index
-    
+    @results ||= []
+  end
+
+  def search
+    @input_name = params[:query]
+    input_words = @input_name.split
+
+    negative_terms = input_words.select { |term| term.start_with?('-') }.map { |term| term[1..-1] }
+    positive_terms = input_words.reject { |term| term.start_with?('-') }
+
+    @results = @data.select do |entry|
+      entry_values = [entry["Name"], entry["Type"], entry["Designed by"]].compact
+      positive_match = positive_terms.all? do |term|
+        entry_values.any? { |value| value && value.downcase.include?(term.downcase) }
+      end
+      negative_match = negative_terms.none? do |term|
+        entry_values.any? { |value| value && value.downcase.include?(term.downcase) }
+      end
+      positive_match && negative_match
+    end
+
+    render 'index'
+  end
+
+  private
+
+  def load_data
+    file_path = Rails.root.join('public', 'data.json')
+    @data = JSON.parse(File.read(file_path))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
-  
   resources :main, only: %i[index]
   root to: 'main#index'
+
+  get '/search', to: 'main#search', as: :search
 end
