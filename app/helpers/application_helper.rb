784865486da658981e20b6fd9f4166a3b1b1a026
Tarasukk <relaxsonik@gmail.com>
2024-01-06 11:56:18 +0200

    Added the highlight_search_term method to enhance search result visualization.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def highlight_search_term(value)
+    return value if value.blank? || @results.blank?
+  
+    input_words = params[:query].split
+    input_words.each do |word|
+      escaped_word = Regexp.escape(word)
+      value.gsub!(Regexp.new("(?i)#{escaped_word}"), "<span class='bg-warning'>#{word}</span>")
+    end
+    value.html_safe
+  end
+  
 end
